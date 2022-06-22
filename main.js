b8dd741349735f606b7c90723fd7b6a13d3d390a
gayatrieee <88025844+gayatrieee@users.noreply.github.com>
2022-06-22 15:09:23 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+function addRoom()
+{
+    room_name = document.getElementById("room_name").value;
+
+
+    firebase.database().ref("/").child(room_name).update({
+        purpose : "adding room name"
+    });
+
+    localStorage.setItem("room_name", room_name);
+
+
+    window.location = "kwitter_page.html";
+    }
+    function getData () {firebase.database().ref("/").on('value',function(snapshot) {document.getElementById("output").innerHTML
+    Room_names = childKey;
+    console.log ("Room name - " + Room_names);
+    row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+    document.getElementById("output").innerHTML += row;
+  
+   getData();
+
+function redirectToRoomName(name)
+{
+    console.log(name);
+    localStorage.setItem("room_name", name);
+     window.location = "kwitter_page.html";
+    }
+    function logout() {
+        localStorage.removeItem("user_name");
+        localStorage.removeItem("room_name");
+         window.location = "kwitter.html";
+         }
+        function send()
+        {
+            msg= document.getElementById("msg").value;
+            firebase.database().ref(room_name).push({
+                name:user_name
+                message:msg,
+                like:0
+            });
+            document.getElementById("msg").value= "";
+          }
+          firebase.database().ref(room_name).push({
+              name:user_name,
+              message:msg,
+              like:0
+          });
+          function updateLike(message_id)
+          {
+                console.log("clicked on like button - " + message_id);
+               button_id = message_id;
+               likes = document.getElementById(button_id).value;
+               updated_like = Number(likes) +1;
+               console.log(updated_likes);
+          
+               firebase.database().ref(room_name).child(message_id).update({
+                     like : updated_likes
+                     });
+                  }
+          function logout() {
+              localStorage.removeItem("user_name");
+              localStorage.removeItem("room_name");
+              window.location.replace("kwitter.html");
+              }
